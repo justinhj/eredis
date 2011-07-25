99e8cd11bae55305210b1bd409863d9ea923984f
justinhj <justinhj@f337d84f-f73c-1c5d-8159-7da666b392c2>
2011-07-25 04:27:35 +0000

    some tidy up

--- eredis.el
+++ eredis.el
@@ -1,36 +1,16 @@
 ;; eredis... A simple emacs interface to redis
-;; http://redis.io/topics/protocol
-;; see unified protocol to see what's going on in the command sends
-;; basically * + number of strings then the length of each string preceded by $
-;; todo generalise command sending 
-;; todo data structure representing API and expected response
+;; See for info on the protocol http://redis.io/topics/protocol
 
-;; usage examples
-;; first connect to a server
-; (redis-hai host port nowait) 
-; last arg is whether to wait for connection or connect asynchronously. defaults to nil 
-; (redis-hai "127.0.0.1" 6379) 
-; (redis-hai "127.0.0.1" 6379 t)
-; close connection
-; (redis-kthxbye) 
-; returns true if you can ping redis
-; (redis-ping)
-; (redis-info)
-; (redis-get "user:66971:last-login")
-; for reference
-; (process-list)
-; (list-processes)
-; (delete-process (get-process "redis"))
-; (network-interface-list)
-; (network-interface-info "en1")	  
-;(insert (synchronous-ping))
+;; By Justin Heyes-Jones 2011
+;; This is released under the Gnu License v3. See http://www.gnu.org/licenses/gpl.txt
 
+;; I addded support for editing and viewing keys as an org-table
 (require 'org-table)
 
-(defvar *redis-process* nil)
-(defvar *redis-state* nil)
-(defvar *redis-response* nil)
-(defvar *redis-timeout* 10) ; seconds 
+(defvar *redis-process* nil "Current Redis client process")
+(defvar *redis-state* nil "Statue of the connection")
+(defvar *redis-response* nil "Stores response of last Redis command")
+(defvar *redis-timeout* 10 "Timeout on the client in seconds when waiting for Redis")
 
 ;; UTILS
 
@@ -43,93 +23,115 @@
 
 ;; helper function from http://www.emacswiki.org/emacs/ElispCookbook#toc5
 (defun chomp (str)
-  "chomp leading and tailing whitespace from STR."
+  "Remove leading and tailing whitespace from STR."
   (let ((s (if (symbolp str) (symbol-name str) str)))
     (replace-regexp-in-string "\\(^[[:space:]\n]*\\|[[:space:]\n]*$\\)" "" s)))
 
 (defun insert-map(m)
-  "insert a map M of key value pairs into the buffer"
+  "insert a map M of key value pairs into the current buffer"
   (maphash (lambda (a b) (insert (format "%s,%s\n" a b))) m))
 
-(defun redis-map-keys(key-expr)
-  "take a glob expression and return the key/values of matching keys"
-  (let ((keys (redis-keys key-expr)))
+(defun eredis-map-keys(key-expr)
+  "take a glob expression like \"user.id.*\" and return the key/values of matching keys"
+  (let ((keys (eredis-keys key-expr)))
     (if keys
-	(let ((values (redis-mget keys)))
+	(let ((values (eredis-mget keys)))
 	  (two-lists-to-map keys values))
       nil)))
 
-(defun redis-buffer-message(process message)
+(defun eredis-buffer-message(process message)
   "print a message to the redis process buffer"
   (save-excursion 
     (set-buffer (process-buffer process))
     (insert message)))
 
-(defun redis-sentinel(process event)
+(defun eredis-sentinel(process event)
   "sentinal function for redis network process which monitors for events"
-  (redis-buffer-message process (format "sentinel event %s" event))
+  (eredis-buffer-message process (format "sentinel event %s" event))
   (cond 
    ((string-match "open" event)
     (setq *redis-state* 'open))))
 
-(defun redis-filter(process string)
+(defun eredis-filter(process string)
   "filter function for redis network process, which receives output"
   (setq *redis-response* string))
 
-(defun redis-delete-process()
+(defun eredis-delete-process()
   (when *redis-process*
     (delete-process *redis-process*)
     (setq *redis-state* 'closed)))
 
-(defun redis-hai(host port &optional no-wait)
-  (redis-delete-process)
+(defun eredis-hai(host port &optional no-wait)
+  (interactive "sHost: \nsPort: \n")
+  (eredis-delete-process)
   (setq *redis-state* 'opening)
   (let ((p 
 	 (make-network-process :name "redis"
 			       :host host
 			       :service port
 			       :nowait no-wait
-			       :filter #'redis-filter
+			       :filter #'eredis-filter
 			       :keepalive t
 			       :linger t
-			       :sentinel #'redis-sentinel
+			       :sentinel #'eredis-sentinel
 			       :buffer (get-buffer-create "*redis*"))))
     (if p
 	(progn
+	  ;; When doing a blocking connect set the state to
+	  ;; open. A non-nlocking connect will set the state 
+	  ;; to open when the connection calls the sentinel
 	  (if (null no-wait)
-	      (setf *redis-state* 'open))
+	      (progn
+		(when (called-interactively-p)
+		  (message "Redis connected"))
+		(setf *redis-state* 'open)))
 	  (setf *redis-process* p)))))
      
-(defun redis-kthxbye()
-  (redis-delete-process))
+(defun eredis-kthxbye()
+  "Close the connection to Redis"
+  (interactive)
+  (eredis-delete-process))
 
-(defun redis-ping()
+(defun eredis-ping()
+  "Return true if you can ping the Redis server"
+  (interactive)
   (if (and *redis-process* (eq *redis-state* 'open))
       (progn 
 	(process-send-string *redis-process* "PING\r\n")
-	(let ((resp (get-redis-response)))
+	(let ((resp (eredis-get-response)))
 	  (if (string-match "+PONG" resp)
-	    t
+	      (progn
+		(when (called-interactively-p)
+		  (message "Pong"))
+		t)
 	    nil)))))
 
-(defun redis-get(key)
+(defun eredis-get(key)
   (if (and *redis-process* (eq *redis-state* 'open))
       (process-send-string *redis-process* (format "GET %s\r\n" key))))
 
-(defun redis-info()
+(defun eredis-parse-bulk(resp)
+  "parse the redis bulk response RESP and return the result"
+  (let ((count (string-to-number (subseq resp 1))))
+    (let ((body-start (string-match "\r\n" resp)))
+      (when body-start
+	(subseq resp body-start (+ count body-start))))))
+
+(defun eredis-info()
+  (interactive)
   (if (and *redis-process* (eq *redis-state* 'open))
       (progn
 	(process-send-string *redis-process* "INFO\r\n")
-	(let ((resp (get-redis-response)))
-	  (insert resp))))) ; todo need to handle bulk reply (simpler version of multi bulk)
+	(let ((resp (eredis-get-response)))
+	  (eredis-buffer-message *redis-process* (eredis-parse-bulk resp))))))
 
-(defun get-redis-response()
+(defun eredis-get-response()
   "await response from redis and store it"
   (if (accept-process-output *redis-process* *redis-timeout* 0 t)
       *redis-response*
     nil))
 
-(defun redis-parse-multi-bulk(resp)
+(defun eredis-parse-multi-bulk(resp)
   "parse the redis multi bulk response RESP and return the list of results"
   (if (< (length resp) 2)
       nil
@@ -145,20 +147,22 @@
 
 ; http://redis.io/commands/keys
 
-(defun redis-keys(pattern)
+(defun eredis-keys(pattern)
   "returns a list of keys where the key matches the provided
 pattern. see the link for the style of patterns"
   (process-send-string *redis-process* (concat "KEYS " pattern "\r\n"))
-  (let ((r (get-redis-response)))
-    (redis-parse-multi-bulk r)))
+  (let ((r (eredis-get-response)))
+    (eredis-parse-multi-bulk r)))
 
-(defun redis-mget(keys)
+; http://redis.io/commands/mget
+
+(defun eredis-mget(keys)
   "return the values of the specified keys, or nil if not present"
   (process-send-string *redis-process* (concat "MGET " (mapconcat 'identity keys " ") "\r\n"))
-  (let ((r (get-redis-response)))
-    (redis-parse-multi-bulk r)))
+  (let ((r (eredis-get-response)))
+    (eredis-parse-multi-bulk r)))
 
-(defun redis-get-map(keys)
+(defun eredis-get-map(keys)
   "given a map M of key/value pairs, go to Redis to retrieve the values and set the 
 value to whatever it is in Redis (or nil if not found)"
   (let ((num-args (1+ (hash-table-count m))))
@@ -168,19 +172,19 @@ value to whatever it is in Redis (or nil if not found)"
 		 (setf key-value-string (concat key-value-string (format "$%d\r\n%s\r\n" (length k) k))))
 	       m)
       (process-send-string *redis-process* (concat command key-value-string))
-      (get-redis-response)))
+      (eredis-get-response)))
 
-(defun redis-set(k v)
+(defun eredis-set(k v)
   "set the key K and value V in Redis"
   (let ((command "*3\r\n$3\r\nSET\r\n")
 	(key-value-string (format "$%d\r\n%s\r\n$%d\r\n%s\r\n" (length k) k (length v) v)))
     (process-send-string *redis-process* (concat command key-value-string))
-    (let ((resp (get-redis-response)))
+    (let ((resp (eredis-get-response)))
       (if (string-match "+OK" resp)
 	  t
 	nil))))
 
-(defun redis-mset(m)
+(defun eredis-mset(m)
   "set the keys and values of the map M in Redis"
   (let ((num-args (1+ (* 2 (hash-table-count m)))))
     (let ((command (format "*%d\r\n$4\r\nMSET\r\n" num-args))
@@ -189,12 +193,14 @@ value to whatever it is in Redis (or nil if not found)"
 		 (setf key-value-string (concat key-value-string (format "$%d\r\n%s\r\n$%d\r\n%s\r\n" (length k) k (length v) v))))
 	       m)
       (process-send-string *redis-process* (concat command key-value-string))
-      (let ((resp (get-redis-response)))
+      (let ((resp (eredis-get-response)))
 	(if (string-match "+OK" resp)
 	    t
 	  nil)))))
 
-(defun redis-mset-region(beg end delimiter) 
+(defun eredis-mset-region(beg end delimiter) 
+  "Parse the current region using DELIMITER to split each line into a key value pair which
+is then sent to redis using mset"
   (interactive "*r\nsDelimiter: ")
   (let ((done nil)
 	(mset-param (make-hash-table :test 'equal)))
@@ -209,32 +215,32 @@ value to whatever it is in Redis (or nil if not found)"
 		  delimiter)))
 	    (let ((key (first split-line))
 		  (value (second split-line)))
-	      (if (or 
-		   (null key)
-		   (null value))
+	      (if (or (null key) (null value))
 		  (setf done t)
 		(progn
 		  (puthash key value mset-param)
 		  (next-line))))))))
     (if (> (hash-table-count mset-param) 0)
-	(redis-mset mset-param)
+	(eredis-mset mset-param)
       nil)))
 
-(defun redis-org-table-from-pattern(pattern)
+(defun eredis-org-table-from-pattern(pattern)
+  "Search Redis for the pattern of keys and create an org table from the results"
   (interactive "sPattern: ")
-  (let ((m (redis-map-keys pattern)))
+  (let ((m (eredis-map-keys pattern)))
     (if m
 	(org-table-from-map m)
       (message (format "No keys found for pattern %s" pattern)))))
 
 (defun org-table-from-map(m)
+  "Create an org-table from a map of key value pairs"
   (let ((beg (point)))
     (if (hash-table-p m)
 	(progn
 	  (insert-map m)
 	  (org-table-convert-region beg (point))))))
 
-(defun redis-org-table-get-field-clean(col)
+(defun eredis-org-table-get-field-clean(col)
   "Get a field in org table at column COL and strip any leading or
 trailing whitespace using chomp. Also strip text properties"
   (let ((field (org-table-get-field col)))
@@ -242,7 +248,7 @@ trailing whitespace using chomp. Also strip text properties"
       (set-text-properties 0 (length chomped) nil chomped)
       chomped)))
 
-(defun redis-org-table-to-map()
+(defun eredis-org-table-to-map()
   "Walk an org table and convert the first column to keys and the second 
 column to values in an elisp map"
   (let ((retmap (make-hash-table :test 'equal)))
@@ -251,38 +257,37 @@ column to values in an elisp map"
 	    (end (org-table-end)))
 	(goto-char beg)
 	(while (> end (point))
-	  (let ((key (redis-org-table-get-field-clean 1))
-		(value (redis-org-table-get-field-clean 2)))
+	  (let ((key (eredis-org-table-get-field-clean 1))
+		(value (eredis-org-table-get-field-clean 2)))
 	    (when (and key value)
 	      (puthash key value retmap)))
 	  (next-line))))
     retmap))
 
-(defun redis-org-table-row-to-key-value-pair()
+(defun eredis-org-table-row-to-key-value-pair()
   "When point is in an org table convert the first column to a key and the second 
 column to a value, returning the result as a dotted pair"
-  (interactive)
   (let ((beg (org-table-begin))
 	(end (org-table-end)))
     (if (and (>= (point) beg)
 	     (<= (point) end))
-	(let ((key (redis-org-table-get-field-clean 1))
-	      (value (redis-org-table-get-field-clean 2)))
+	(let ((key (eredis-org-table-get-field-clean 1))
+	      (value (eredis-org-table-get-field-clean 2)))
 	  (if (and key value)
 	      (cons key value)
 	    nil))
       nil)))
 
-(defun redis-org-table-mset()
+(defun eredis-org-table-mset()
   "with point in an org table convert the table to a map and send it to redis with mset"
   (interactive)
-  (let ((m (redis-org-table-to-map)))
-    (redis-mset m)))
+  (let ((m (eredis-org-table-to-map)))
+    (eredis-mset m)))
 
-(defun redis-org-table-row-set()
+(defun eredis-org-table-row-set()
   "with point in an org table set the key and value"
   (interactive)
-  (let ((keyvalue (redis-org-table-row-to-key-value-pair)))
-    (redis-set (car keyvalue) (cdr keyvalue))))
+  (let ((keyvalue (eredis-org-table-row-to-key-value-pair)))
+    (eredis-set (car keyvalue) (cdr keyvalue))))
 
 (provide 'eredis)
